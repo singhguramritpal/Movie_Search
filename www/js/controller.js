b7e541071b3630d7bd9ea4f2d6225eb092374be4
Guramritpal Singh <singh.guramritpal@yahoo.com>
2016-06-09 03:12:32 -0700

    Update controller.js

--- www/js/controller.js
+++ www/js/controller.js
@@ -21,13 +21,6 @@ app.controller('DashCtrl', function($scope, $http) {
 
 app.controller('ChatDetailCtrl', function($scope, $http, $stateParams) {
 
-  $scope.myValue="false";
-
-  $scope.resizeImage=function(){
-  $scope.myValue="true";
-    alert("hello ");
-  }
-
   $http.get("http://www.omdbapi.com/?i="+$stateParams.imbdID+"&plot=full")
     .then(function(response){
       $scope.chat = response.data;
